403604cfe3e79ac9698d0bbf7283f18ff4c8d094
XinHuiUCD <xinhui.jiang@ucdconnect.ie>
2022-06-20 11:33:20 +0100

    2022/06/20 John: First combine django and vue

--- dublinBus/dublinBus/settings.py
+++ dublinBus/dublinBus/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = ["ipa-011.ucd.ie"]
 # Application definition
 
 INSTALLED_APPS = [
+    'xpressbus.apps.XpressbusConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Dublin'
 
 USE_I18N = True
 
