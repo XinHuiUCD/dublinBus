60d517091f556bfac213deaee3a1b08d40572ec9
XinHuiUCD <xinhui.jiang@ucdconnect.ie>
2022-07-01 12:19:12 +0100

    fix weather.js synchronize problem

--- dbus/src/store/weather.js
+++ dbus/src/store/weather.js
@@ -19,8 +19,8 @@ const ModuleWeather = {
             $.ajax({
                 url: "https://api.openweathermap.org/data/2.5/onecall?lat=53.344&lon=-6.2672&units=metric&exclude=minutely,daily&appid=d6e328f404504a98d4be6d3942d42e9e",
                 type: "GET",
+                async: false,
                 success(resp) {
-                    console.log(resp);
                     context.commit("updateWeather", {
                         temps: resp.hourly,
                         tempsLen: resp.hourly.length,
