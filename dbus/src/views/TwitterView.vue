8e82e24cfffc4d0c5fc59dfddaaacaa645e5e993
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-07-07 18:42:57 +0100

    changing twitter mode

--- dbus/src/views/TwitterView.vue
+++ dbus/src/views/TwitterView.vue
@@ -1,36 +1,32 @@
 <template>
-	<ContentBase>
-		<div id="twitter">
-			<frame-options policy="SAMEORIGIN" />
-			<iframe :src="txtUrl" frameborder="0" width="100%" :style="{height:calHeight}" scrolling="auto"></iframe>
-		</div>
-
-	</ContentBase>
+  <a
+    class="twitter-timeline"
+    :href="src"
+    :data-theme="dark ? 'dark' : 'light'"
+    :data-link-color="dataLinkColor"
+  ></a>
 </template>
 
-<script lang="ts">
-	import ContentBase from '../components/ContentBase.vue'
-
-	export default {
-		name: "TwitterView",
-		components: {
-			ContentBase,
-		},
-		data() {
-			return {
-				txtUrl: "https://twitter.com/dublinbusnews?ref_src=twsrc%5Etfw",
-			}
-		},
-
-		computed: {
-			//Calculate the property and set the iframe height to 100px less than the window height
-			calHeight() {
-				return (window.innerHeight - 100) + 'px';
-			}
-		},
-	}
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: "https://twitter.com/dublinbusnews"
+    },
+    dark: {
+      type: Boolean,
+      default: true
+    },
+    dataLinkColor: {
+      type: String,
+      default: "#2B7BB9"
+    }
+  },
+  created() {
+    let twitterFeed = document.createElement("script");
+    twitterFeed.setAttribute("src", "https://platform.twitter.com/widgets.js");
+    document.head.appendChild(twitterFeed);
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
