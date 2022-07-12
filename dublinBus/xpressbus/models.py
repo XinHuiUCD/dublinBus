c2c10ec73417996b293e9a843cb9a6b59192d572
XinHuiUCD <xinhui.jiang@ucdconnect.ie>
2022-07-12 21:04:24 +0100

    solve merge conflict

--- dublinBus/xpressbus/models.py
+++ dublinBus/xpressbus/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Bus(models.Model):
+    routeId = models.IntegerField()
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    def __str__(self):
+        return self.routeId
