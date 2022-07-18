184a9efeb0c6b43f08054c2adbc87cfa32f79867
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-07-18 10:31:15 +0100

    trying to do real time

--- dublinBus/xpressbus/views.py
+++ dublinBus/xpressbus/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+import pandas as pd
+import json
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+def real_time_bus(request):
+
+    url = "https://www.dublinbus.ie/en/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery=2"
+    # read html table into a data frame
+    df = pd.read_html(url)
+    # print(df)
+
+
+    time_table = df[3].to_json(orient='records')
+
+    return Response(json.loads(time_table))
