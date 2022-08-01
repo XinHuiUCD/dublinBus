d7dd08d72934af59aaba5d04ffc1008b2fcda036
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-08-01 21:15:43 +0100

    unti testing

--- dbus/tests/unit/Map.spec.js
+++ dbus/tests/unit/Map.spec.js
@@ -1,2 +1,16 @@
 import { mount } from '@vue/test-utils';
 import MapDisplay from '@components/MapDisplay.vue'
+
+
+import { shallowMount } from '@vue/test-utils'
+import MapDisplay from '@/components/MapDisplay.vue'
+
+describe('MapDisplay.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(MapDisplay, {
+      props: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
